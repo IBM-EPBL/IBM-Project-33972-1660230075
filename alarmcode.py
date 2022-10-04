dbb0dacabd4c4508c755a5b8884bc76660bba142
Shaik-Kamiloon <77109335+Shaik-Kamiloon@users.noreply.github.com>
2022-10-05 00:33:35 +0530

    Add files via upload
    IBM Assignment 2

--- alarmcode.py
+++ alarmcode.py
@@ -1,9 +1,13 @@
 #IBM Asssignment 2
-
+'''
+Python code to get temperature and humidity values generated with random function to a variable
+and detect an alarm in case of high temperature
+'''
 import random
 import winsound
-temperature= random.randrange(0,100)
-print(temperature)
+temperature = random.randrange(0,100)
+humidity = random.randrange(0,100)
+print("Temperature = ",temperature)
 if(temperature>60):
     print("HIGH TEMPERATURE")
     winsound.Beep(4460, 1000)
@@ -11,8 +15,8 @@ else:
     print("NORMAL TEMPERATURE")
     
 ctr=random.randint(3,8)
-dew_point=temperature-ctr
-print("dew_point = ",dew_point)
-rh=100*(2.71828*(17.625*dew_point/(243.04+dew_point)))/(2.71828*(17.625*temperature/(243.04+temperature)))
-print("Relative Humidity = ",round(rh,2))
+Dew_point=temperature-ctr
+print("Dew_point = ",Dew_point)
+print("Humidity = ",humidity)
+
 
